fbc724e5e8845e8aa91ee4da3c7a57987c946139
rialehemaa <dariia.klimuk17@gmail.com>
2026-02-19 14:08:23 +0200

    ulesanne 7-13

--- Ulesanne/Ulesanne/Program.cs
+++ Ulesanne/Ulesanne/Program.cs
@@ -88,6 +88,227 @@ namespace Ülesanned
             {
                 Console.WriteLine("Vigane otsing, arv on väljaspool tabeli piire.");
             }
+
+            Console.WriteLine("\n Ülesanne 8: Õpilastega mängimine");
+
+            string[] õpilased = { "Anna", "Bert", "Cärol", "Daan", "Eliis", "Fred", "Grete", "Hanna", "Ivo", "Jaak" };
+
+            õpilased[2] = "Kati";
+            õpilased[5] = "Mati";
+
+            Console.WriteLine("\n1. Asendatud nimed:");
+            foreach (var nimi in õpilased)
+            {
+                Console.WriteLine(nimi);
+            }
+
+            Console.WriteLine("\n2. While – nimed mis algavad tähega 'A':");
+            int w = 0;
+            while (w < õpilased.Length)
+            {
+                if (õpilased[w].StartsWith("A"))
+                {
+                    Console.WriteLine($"Tere, {õpilased[w]}!");
+                }
+                w++;
+            }
+
+            Console.WriteLine("\n3. For – kõik nimed ja indeksid:");
+            for (int i = 0; i < õpilased.Length; i++)
+            {
+                Console.WriteLine($"[{i}] {õpilased[i]}");
+            }
+
+            Console.WriteLine("\n4. Foreach – nimed väikeste tähtedena:");
+            foreach (var nimi in õpilased)
+            {
+                Console.WriteLine(nimi.ToLower());
+            }
+
+            Console.WriteLine("\n5. Do-while – tervita kuni nimeni 'Mati':");
+            int dw = 0;
+            do
+            {
+                Console.WriteLine($"Tere, {õpilased[dw]}!");
+                dw++;
+            } while (dw < õpilased.Length && õpilased[dw - 1] != "Mati");
+
+
+            Console.WriteLine("\n Ülesanne 9: Arvude ruudud");
+
+            int[] arvud9 = { 2, 4, 6, 8, 10, 12 };
+
+            Console.WriteLine("\n1. For – iga arvu ruut:");
+            for (int i = 0; i < arvud9.Length; i++)
+            {
+                Console.WriteLine($"{arvud9[i]}² = {arvud9[i] * arvud9[i]}");
+            }
+
+            Console.WriteLine("\n2. Foreach – iga arvu kahekordne väärtus:");
+            foreach (var arv in arvud9)
+            {
+                Console.WriteLine($"{arv} * 2 = {arv * 2}");
+            }
+
+            Console.WriteLine("\n3. While – arvud mis jaguvad 3-ga:");
+            int loendur = 0;
+            int j = 0;
+            while (j < arvud9.Length)
+            {
+                if (arvud9[j] % 3 == 0)
+                {
+                    loendur++;
+                }
+                j++;
+            }
+            Console.WriteLine($"3-ga jaguvaid arve: {loendur}");
+
+
+            Console.WriteLine("\n Ülesanne 10: Positiivsed ja negatiivsed");
+
+            int[] arvud10 = { 5, -3, 0, 8, -1, 4, -7, 2, 0, -5, 6, 9 };
+
+            int positiivsed = 0;
+            int negatiivsed = 0;
+            int nullid = 0;
+
+            foreach (var arv in arvud10)
+            {
+                if (arv > 0)
+                {
+                    positiivsed++;
+                }
+                else if (arv < 0)
+                {
+                    negatiivsed++;
+                }
+                else
+                {
+                    nullid++;
+                }
+            }
+
+            Console.WriteLine($"Positiivseid arve: {positiivsed}");
+            Console.WriteLine($"Negatiivseid arve: {negatiivsed}");
+            Console.WriteLine($"Nulle:             {nullid}");
+
+
+            Console.WriteLine("\n Ülesanne 11: Keskmisest suuremad");
+
+            Random juhus = new Random();
+            int[] arvud11 = new int[15];
+
+            for (int i = 0; i < arvud11.Length; i++)
+            {
+                arvud11[i] = juhus.Next(1, 101);
+            }
+
+            Console.Write("Genereeritud arvud: ");
+            foreach (var arv in arvud11)
+            {
+                Console.Write($"{arv} ");
+            }
+            Console.WriteLine();
+
+            int summa11 = 0;
+            for (int i = 0; i < arvud11.Length; i++)
+            {
+                summa11 += arvud11[i];
+            }
+            double keskmine11 = (double)summa11 / arvud11.Length;
+            Console.WriteLine($"\n1. Keskmine väärtus: {keskmine11:F2}");
+
+            Console.WriteLine("\n2. Keskmisest suuremad arvud:");
+            foreach (var arv in arvud11)
+            {
+                if (arv > keskmine11)
+                {
+                    Console.Write($"{arv} ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\n3. Do-while – arvud kuni esimese alla 10 olevani:");
+            int k11 = 0;
+            do
+            {
+                Console.Write($"{arvud11[k11]} ");
+                k11++;
+            } while (k11 < arvud11.Length && arvud11[k11 - 1] >= 10);
+            Console.WriteLine();
+
+
+            Console.WriteLine("\n Ülesanne 12: Kõige suurema arvu otsing");
+
+            int[] numbrid = { 12, 56, 78, 2, 90, 43, 88, 67 };
+
+            int suurimVäärtus = numbrid[0];
+            int suurimIndeks = 0;
+
+            for (int i = 1; i < numbrid.Length; i++)
+            {
+                if (numbrid[i] > suurimVäärtus)
+                {
+                    suurimVäärtus = numbrid[i];
+                    suurimIndeks = i;
+                }
+            }
+
+            Console.WriteLine($"Suurim arv: {suurimVäärtus}");
+            Console.WriteLine($"Suurima arvu indeks: {suurimIndeks}");
+
+
+            Console.WriteLine("\n Ülesanne 13: Paari- ja paaritud loendused");
+
+            List<int> arvud13 = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                arvud13.Add(juhus.Next(1, 101));
+            }
+
+            Console.Write("Genereeritud arvud: ");
+            foreach (var arv in arvud13)
+            {
+                Console.Write($"{arv} ");
+            }
+            Console.WriteLine();
+
+            long paarisarvudeSumma = 0;
+            for (int i = 0; i < arvud13.Count; i++)
+            {
+                if (arvud13[i] % 2 == 0)
+                {
+                    paarisarvudeSumma += arvud13[i];
+                }
+            }
+            Console.WriteLine($"\nPaarisarvude kogusumma: {paarisarvudeSumma}");
+
+            long paarituteKogusumma = 0;
+            int paarituteArv = 0;
+            foreach (var arv in arvud13)
+            {
+                if (arv % 2 != 0)
+                {
+                    paarituteKogusumma += arv;
+                    paarituteArv++;
+                }
+            }
+            double paarituteKeskmine = paarituteArv > 0 ? (double)paarituteKogusumma / paarituteArv : 0;
+            Console.WriteLine($"Paaritute arvude keskmine: {paarituteKeskmine:F2}");
+
+            int üle50 = 0;
+            int m = 0;
+            while (m < arvud13.Count)
+            {
+                if (arvud13[m] > 50)
+                {
+                    üle50++;
+                }
+                m++;
+            }
+            Console.WriteLine($"Arvud mis on suuremad kui 50: {üle50}");
+
         }
     }
 
