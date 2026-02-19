0a2c3012854f721c9e9bf328b618a0572385c9bb
rialehemaa <dariia.klimuk17@gmail.com>
2026-02-19 16:25:46 +0200

    Osa1

---
+++ Osa1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Startclass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Tere tulemast!");
+
+        Console.Write("Sisesta oma eesnimi: ");
+        string eesnimi = Console.ReadLine();
+        Console.WriteLine("Tere, " + eesnimi);
+
+        Console.Write("Sisesta esimene arv: ");
+        int arv1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Sisesta teine arv: ");
+        int arv2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+
+        Console.ReadLine();
+    }
+}
