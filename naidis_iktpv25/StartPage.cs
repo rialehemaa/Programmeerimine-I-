7fdefd51a9778eaaddb9387efb1b53349b1cdd35
rialehemaa <dariia.klimuk17@gmail.com>
2026-02-28 17:36:54 +0200

    Naidis_IKTpv25

---
+++ naidis_iktpv25/StartPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace naidis_iktpv25
+{
+    class StartPage
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine("PEAMENÜÜ ");
+                Console.WriteLine("1 - Osa 2 ülesanded");
+                Console.WriteLine("2 - Osa 3 ülesanded");
+                Console.WriteLine("0 - Välju");
+                Console.Write("Valik: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Osa2_funktsioonid.Menuu();
+                        break;
+                    case "2":
+                        Osa3_funktsioonid.Menuu();
+                        break;
+                    case "0":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Vale valik!");
+                        break;
+                }
+            }
+        }
+    }
+}
