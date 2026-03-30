0db1025d9faf66fb2b32244f8738c90b8d6bb015
Opilane TTHK <dariia.klimuk17@gmail.com>
2026-03-30 11:08:33 +0300

    5Osa-Teooria-Ulesanded

---
+++ 5OsaFunkTeor/Opilane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5OsaFunkTeor
+
+{
+    internal class Opilane
+    {
+        public string Nimi;
+        public List<int> Hinded = new List<int>();
+
+        public double Keskmine()
+        {
+            double summa = 0;
+
+            foreach (int h in Hinded)
+                summa += h;
+
+            return summa / Hinded.Count;
+        }
+    }
+}
---
+++ 5OsaFunkTeor/Valuta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5OsaFunkTeor
+{
+    internal class Valuuta
+    {
+        public string Nimetus;
+        public double KurssEurSuhte;
+    }
+}
