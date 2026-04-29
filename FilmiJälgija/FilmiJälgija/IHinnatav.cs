70a1d900f1d9d638fd55c466742f4f7a0532f80f
rialehemaa <dariia.klimuk17@gmail.com>
2026-04-29 15:14:11 +0300

    FilmiJälgija OPP

---
+++ FilmiJälgija/FilmiJälgija/IHinnatav.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmiJälgija
+{
+    public interface IHinnatav
+    {
+        void LisaHinnang(double hinnang);
+        void KuvaInfo();
+    }
+}
---
+++ FilmiJälgija/FilmiJälgija/Meedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmiJälgija
+{
+    public abstract class Meedia
+    {
+        public string Pealkiri { get; set; }
+        public int Aasta { get; set; }
+        public string Staatus { get; set; }
+        public double Hinnang { get; set; }
+        public string Märkus { get; set; }
+
+        public abstract void KuvaInfo();
+    }
+}
